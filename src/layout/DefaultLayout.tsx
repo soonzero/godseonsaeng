c9bd35b48e3683d8010a9aab62c84ac158d1fcb4
soonzero <sykeum0912@naver.com>
2023-02-04 00:03:57 +0900

    :lipstick: 레이아웃 수정

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,14 +1,22 @@
+import styled from 'styled-components';
 import Footer from 'components/common/Footer';
 import Header from 'components/common/Header';
 import Main from 'components/common/Main';
 
+const Layout = styled.div`
+	display: flex;
+	flex-direction: column;
+
+	min-height: 100vh;
+`;
+
 const DefaultLayout = () => {
 	return (
-		<>
+		<Layout>
 			<Header />
 			<Main />
 			<Footer />
-		</>
+		</Layout>
 	);
 };
 
